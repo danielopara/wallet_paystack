9275308fcc7e40c32e78ef8266070bb95eab6a9d
DanielOpara <oparadaniv@gmail.com>
2025-03-30 21:25:11 +0100

    adding auth for create transaction

--- api/wallet/views.py
+++ api/wallet/views.py
@@ -1,5 +1,6 @@
 from api.wallet.service import WalletService
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['POST'])
@@ -7,5 +8,6 @@ def create_wallet(request):
     return WalletService().create_wallet(request)
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def create_transaction(request):
     return WalletService().create_transaction(request)
