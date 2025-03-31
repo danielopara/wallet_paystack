4a55936cc120cd391c2d003bbebdb3314f614749
DanielOpara <oparadaniv@gmail.com>
2025-03-31 20:17:28 +0100

    fixed invalid token error

--- api/middlewares.py
+++ api/middlewares.py
@@ -3,21 +3,28 @@ from django.http import JsonResponse
 from django.utils.deprecation import MiddlewareMixin
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import AuthenticationFailed, InvalidToken
 
 
 class AuthMiddleware(MiddlewareMixin):
     def process_request(self, request):
         auth = JWTAuthentication()
-        auth_result = auth.authenticate(request)
 
-        if auth_result is None:
-            request.wallet_profile = None
-            return
-
-        user, _ = auth_result
-
+        
         try:
-            profile = Wallet.objects.get(user=user)
-            request.wallet_profile = profile  # Attach profile to request
-        except Wallet.DoesNotExist:
-            return JsonResponse({"message": "User profile not found"}, status=404)
+            auth_result = auth.authenticate(request)
+            if auth_result is not None:
+                user, _ = auth_result
+                try:
+                    profile = Wallet.objects.get(user=user)
+                    request.wallet_profile = profile
+                except Wallet.DoesNotExist:
+                    request.wallet_profile = None
+            else:
+                request.wallet_profile = None
+        except (InvalidToken, AuthenticationFailed):
+            # Just mark as unauthenticated instead of returning response
+            request.wallet_profile = None
+            
+        # Important: don't return a response here
+        return None
